05593a95256abddd65fb5b3d7d6a4baf24764701
catceli <catherinedcelio@gmail.com>
2020-02-14 14:07:38 -0800

    updated profiles (fix button orientation)

---
+++ puzzles/Julia.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { Dimensions, StyleSheet, Text, View, AppRegistry, Button, Alert, TouchableHighlight, Image } from 'react-native';
+
+export default class Julia extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      puzzle: "Julia"
+    }
+  }
+
+  render(){
+    return (
+      <View style={[ styles.container ]}>
+        <View style = {[ styles.box, { flex: 1 } ]}>
+          <View style = {[ styles.circle ]}>
+          <Image
+            source={require('../images/Julia.jpg')}
+            style={{ flex: 1, width: null, height: null, borderRadius: 90 }}
+            resizeMode='cover'
+          />
+          </View>
+          <Text style = {[ styles.text ]}>Julia Robinson</Text>
+        </View>
+        <View style = {[ styles.box, { flex: 1.4 } ]}>
+        </View>
+        <View style = {[ styles.box, { flex: 0.6, flexDirection: 'row' } ]}>
+        <TouchableHighlight
+          style = {[ styles.button, { flex: 1 } ]}
+          onPress = {() => Alert.alert('hi')}>
+          <Text style = {[ styles.buttonText ]}>Back</Text>
+        </TouchableHighlight>
+          <TouchableHighlight
+            style = {[ styles.button, { flex: 1 } ]}
+            onPress = {() => Alert.alert('hi')}>
+            <Text style = {[ styles.buttonText ]}>Next</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'stretch',
+  },
+  box: {
+    alignItems: 'center',
+    height: 50,
+  },
+  circle: {
+    width: 180,
+    height: 180,
+    borderRadius: 90,
+    justifyContent: 'center',
+    marginTop: 60,
+    marginBottom: 20,
+    backgroundColor: 'red'
+  },
+  text: {
+    color: '#100812',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 30,
+  },
+  button: {
+    alignContent: 'space-around',
+    margin: 30,
+  },
+  buttonText: {
+    flexDirection: 'row',
+    backgroundColor: '#4169E1',
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    overflow: 'hidden',
+    padding: 8,
+    textAlign:'center'
+  },
+});
